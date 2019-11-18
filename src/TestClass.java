970ecf7169cc01badc644722152da6c4546b07ac
user <user@RCR-R90FSB7N>
2019-11-18 17:17:49 -0500

    Removed Old Classes From Package- Smit

--- src/TestClass.java
+++
@@ -1,5 +0,0 @@
-
-public class TestClass {
-	System.out.println("This is a test file! ");
-	
-}
--- src/TestController.java
+++
@@ -1,9 +0,0 @@
-
-public class TestController {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
